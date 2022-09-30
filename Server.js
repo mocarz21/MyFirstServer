57348598014273463e0c29536582c71e569d0152
Adam Mocarski <adammocarski22@gmail.com>
2022-09-30 13:27:43 +0200

    Add server, views home,about, 404, endpoint

---
+++ Server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { userInfo } = require('os');
+const path = require('path')
+
+const app = express(0);
+
+app.use((req,res,next)=>{
+    res.show=(name)=>{
+        res.sendFile(path.join(__dirname, `/views/${name}`))
+    }
+    next();
+})
+
+app.use('/user',(req,res,next) => {
+    if(isUser()) next();
+    else res.show('LogIn.html') //czemu nie działa przejscie do strony
+})
+
+app.use(express.static(path.join(__dirname, '/views/public/')));
+
+app.get('/', (req, res)=>{
+    res.show('index.html');
+})
+app.get('/about', (req, res) => {
+    res.show('about.html');
+})
+
+app.use((req,res) => {
+    res.status(404).show('/public/obraz.jpg');
+})
+
+app.listen(8000, () => {
+    console.log('Server on: 8000');
+})
